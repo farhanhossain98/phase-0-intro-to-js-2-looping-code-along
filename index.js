b1eafc2faa4ade100ae6cf1bf886904257bb718d
farhan hossain <farhanrufon99@gmail.com>
2023-07-02 19:45:04 -0400

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+    function writeCards(names, event) {
+    const newarray = [];
+    for (let i = 0; i < names.length; i++) {
+        newarray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return newarray; 
+    }
+
+   // function countDown(callCount) {
+   //     const newarray = []
+   //     let i=11; 
+   //     while (i > 0) {
+   //         console.log(i > 0)
+   //     i--;
+   //     }
+   //     return newarray; 
+   //
+   // }
+function countDown() {
+   let countDown = 10;
+   while (countDown >= 0) {
+    console.log(countDown--);
+   }
+}
+
